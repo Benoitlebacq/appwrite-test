64cf4e150848f77eef19bd151a381357b0192132
benoit lebacq <benoit.lebacq@ext.weldom.fr>
2024-12-18 10:56:06 +0100

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,37 @@
+import { redirect } from 'next/navigation';
+
+import auth from '@/auth';
+
+export default async function () {
+  const user = await auth.getUser();
+  if (user) redirect('/');
+  return (
+    <div>
+      <form action={auth.createSession} id="login-form">
+        <h3>Login</h3>
+        <p>Enter your information to create an account</p>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your email..."
+            defaultValue="dennis@email.com"
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter your password..."
+            defaultValue="password123"
+          />
+        </div>
+        <div>
+          <input type="submit" value={'Login'} />
+        </div>
+      </form>
+    </div>
+  );
+}
